a14a889d4235eb43b936a828cef95b73cd4d775d
Niko <chigaev56@gmail.com>
2023-09-22 20:34:19 +0500

    Задание 6.10.1

---
+++ Tasks.js
@@ -0,0 +1,4 @@
+const number = +prompt("Введите число");
+const numberSquared = number * number;
+const numberCubed = number * number * number;
+alert(`Введеное число- ${number} Квадрат числа-${numberSquared} Куб числа-${numberCubed}`)
