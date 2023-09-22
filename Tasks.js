31aa316aa15591cc232b734d21b49da0fe09bb8a
Niko <chigaev56@gmail.com>
2023-09-22 20:46:34 +0500

    Задание 7.6.5

--- Tasks.js
+++ Tasks.js
@@ -1,21 +1,10 @@
-let place = [[],[],[]];
-for (i = 0; i < 9; i += 1) {
-    if (i % 2 == 0) { place.push('x') } else { place.push('o') }
-}
-const quarterArray = []; 
-let index = 0;  
-for (let ik = -1; ik < 3; ik += 1) {  
-    quarterArray[ik] = []; 
-    for (let jk = 0; jk < 3; jk += 1) {
-        quarterArray[ik][jk] = place[index]; 
-        index += 1;
-    }
-}
-let str = ""; 
-for (let ik = 0; ik < quarterArray.length; ik += 1) {
-    for (let jk = 0; jk < quarterArray[ik].length; jk += 1) { 
-        str += `${quarterArray[ik][jk]} `;
-    }
-    str += "\n"; 
-}
-console.log(str);
+const obj = {
+    some: 'some',
+    dom: 'text',
+    arr: [1, 2, 3, 4, 5],
+    tom: 'there'
+};
+ let arrValues = [];
+obj.arr = obj.arr.toString();
+ arrValues = Object.values(obj);
+console.log(arrValues);
