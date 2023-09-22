b95c241b969ef59f1656876bc8657b0551e4c05e
Niko <chigaev56@gmail.com>
2023-09-22 20:45:21 +0500

    Задание 7.6.4

--- Tasks.js
+++ Tasks.js
@@ -1,4 +1,21 @@
-let number = prompt("Введите число");
-number = Number(number);
-let numbers = [];
-for (num = 0; num <= number; num += 1) {numbers.push(num)}
+let place = [[],[],[]];
+for (i = 0; i < 9; i += 1) {
+    if (i % 2 == 0) { place.push('x') } else { place.push('o') }
+}
+const quarterArray = []; 
+let index = 0;  
+for (let ik = -1; ik < 3; ik += 1) {  
+    quarterArray[ik] = []; 
+    for (let jk = 0; jk < 3; jk += 1) {
+        quarterArray[ik][jk] = place[index]; 
+        index += 1;
+    }
+}
+let str = ""; 
+for (let ik = 0; ik < quarterArray.length; ik += 1) {
+    for (let jk = 0; jk < quarterArray[ik].length; jk += 1) { 
+        str += `${quarterArray[ik][jk]} `;
+    }
+    str += "\n"; 
+}
+console.log(str);
