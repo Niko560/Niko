1065f47f87ddf74917593cddc28f37388d2695cf
Niko <chigaev56@gmail.com>
2023-09-22 20:38:04 +0500

    Задание 6.10.4

--- Tasks.js
+++ Tasks.js
@@ -1,4 +1,8 @@
 const name = prompt("Введите ваше имя");
 const birthday = prompt("Введите ваш год рождения");
-let age = 2023 - birthday;
-alert(`${name}:${age}`)
+const age = 2023 - birthday;
+const ageRemainder = age % 10;
+const ageFirst = (age - ageRemainder) / 10;
+if (ageRemainder === 1) { alert(`${name}:${ageFirst + "1"}`) }
+else if (ageRemainder === 2, 3, 4) { alert(`${name}:${ageFirst + "4"}`) }
+else { alert(`${name}:${ageFirst + "7"}`) }
