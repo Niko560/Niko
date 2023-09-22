f3ba8600ab9885db49a34b41f4db1d6d463a38fa
Niko <chigaev56@gmail.com>
2023-09-22 20:42:23 +0500

    Задание 7.6.2

--- Tasks.js
+++ Tasks.js
@@ -1,3 +1,3 @@
-const text = prompt("Проверка слова на полидромность");
-let polindrome = text.toLowerCase() === text.split("").reverse().join("").toLowerCase();
-if (polindrome === true) {alert(`Слово ${text} является полиндромом`)} else {alert(`Слово ${text} не является полиндромом`)};
+const arr = [1, 2, 3, 1, 5, 4, 2, 3, 5, 'they', 'don\'t', 'know', 'that', 'we', 'know', 'that', 'they', 'know' ];
+const uniqueNames = new Set(arr);
+console.log(Array.from(uniqueNames));
