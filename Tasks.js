a1d72eb0a172bcd5a8ab9e8f6b11e4b78f68afae
Niko <chigaev56@gmail.com>
2023-09-22 20:40:06 +0500

    Задание 7.6.1

--- Tasks.js
+++ Tasks.js
@@ -1,8 +1,3 @@
-const age = prompt(`Ваш возраст`);
-if (age < 18) { alert(`Кредит недоступен`) } else if
-    (18 <= age && age <= 21) { alert(`Вам доступно до 50.000`) }
-else if
-    (22 <= age && age <= 35) { alert(`Вам доступно до 400.000`) }
-else if
-    (36 <= age && age <= 65) { alert(`Вам доступно до 1.000.000`) }
-else { alert(`Вам кредит не нужен`) }
+const text = prompt("Проверка слова на полидромность");
+let polindrome = text.toLowerCase() === text.split("").reverse().join("").toLowerCase();
+if (polindrome === true) {alert(`Слово ${text} является полиндромом`)} else {alert(`Слово ${text} не является полиндромом`)};
